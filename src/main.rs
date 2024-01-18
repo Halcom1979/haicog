26f89aba7c4b271d4917548d9cf51087303b579d
Mike Kanzler <mike_kanzler@trimble.com>
2024-01-18 16:21:36 +0100

    basic hello world window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run();
 }
+

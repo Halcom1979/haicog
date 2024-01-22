3214654130fdde412bb209b48bce6084acd5a20e
Mike Kanzler <mike_kanzler@trimble.com>
2024-01-22 18:35:34 +0100

    added fpscounter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,154 @@
 use bevy::prelude::*;
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, DiagnosticsStore};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(FrameTimeDiagnosticsPlugin::default())
         .add_systems(Update, bevy::window::close_on_esc)
+        .add_systems(Startup, setup)
+        .add_systems(Startup, setup_fps_counter)
+        .add_systems(Update, (fps_text_update_system, fps_counter_showhide,))
         .run();
 }
 
+/// Marker to find the container entity so we can show/hide the FPS counter
+#[derive(Component)]
+struct FpsRoot;
+
+/// Marker to find the text entity so we can update it
+#[derive(Component)]
+struct FpsText;
+
+fn setup_fps_counter(
+    mut commands: Commands,
+) {
+    // create our UI root node
+    // this is the wrapper/container for the text
+    let root = commands.spawn((
+        FpsRoot,
+        NodeBundle {
+            // give it a dark background for readability
+            background_color: BackgroundColor(Color::BLACK.with_a(0.5)),
+            // make it "always on top" by setting the Z index to maximum
+            // we want it to be displayed over all other UI
+            z_index: ZIndex::Global(i32::MAX),
+            style: Style {
+                position_type: PositionType::Absolute,
+                // position it at the top-right corner
+                // 1% away from the top window edge
+                right: Val::Percent(1.),
+                top: Val::Percent(1.),
+                // set bottom/left to Auto, so it can be
+                // automatically sized depending on the text
+                bottom: Val::Auto,
+                left: Val::Auto,
+                // give it some padding for readability
+                padding: UiRect::all(Val::Px(8.0)),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+    )).id();
+
+    // create our text
+    let text_fps = commands.spawn((
+        FpsText,
+        TextBundle {
+            // use two sections, so it is easy to update just the number
+            text: Text::from_sections([
+                TextSection {
+                    value: "FPS: ".into(),
+                    style: TextStyle {
+                        font_size: 16.0,
+                        color: Color::WHITE,
+                        // if you want to use your game's font asset,
+                        // uncomment this and provide the handle:
+                        // font: my_font_handle
+                        ..default()
+                    }
+                },
+                TextSection {
+                    value: " N/A".into(),
+                    style: TextStyle {
+                        font_size: 16.0,
+                        color: Color::WHITE,
+                        // if you want to use your game's font asset,
+                        // uncomment this and provide the handle:
+                        // font: my_font_handle,
+                        ..default()
+                    }
+                },
+            ]),
+            ..Default::default()
+        },
+    )).id();
+    commands.entity(root).push_children(&[text_fps]);
+}
+
+fn fps_text_update_system(
+    diagnostics: Res<DiagnosticsStore>,
+    mut query: Query<&mut Text, With<FpsText>>,
+) {
+    for mut text in &mut query {
+        // try to get a "smoothed" FPS value from Bevy
+        if let Some(value) = diagnostics
+            .get(FrameTimeDiagnosticsPlugin::FPS)
+            .and_then(|fps| fps.smoothed())
+        {
+            // Format the number as to leave space for 4 digits, just in case,
+            // right-aligned and rounded. This helps readability when the
+            // number changes rapidly.
+            text.sections[1].value = format!("{value:>4.0}");
+            text.sections[1].style.color = Color::WHITE;
+        } else {
+            // display "N/A" if we can't get a FPS measurement
+            // add an extra space to preserve alignment
+            text.sections[1].value = " N/A".into();
+            text.sections[1].style.color = Color::WHITE;
+        }
+    }
+}
+
+/// Toggle the FPS counter when pressing F12
+fn fps_counter_showhide(
+    mut q: Query<&mut Visibility, With<FpsRoot>>,
+    kbd: Res<Input<KeyCode>>,
+) {
+    if kbd.just_pressed(KeyCode::F12) {
+        let mut vis = q.single_mut();
+        *vis = match *vis {
+            Visibility::Hidden => Visibility::Visible,
+            _ => Visibility::Hidden,
+        };
+    }
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+) {
+    // light
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(50.0, 50.0, 50.0),
+        point_light: PointLight {
+            intensity: 600000.,
+            range: 100.,
+            ..default()
+        },
+        ..default()
+    });
+
+    // camera
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 8.0).looking_at(Vec3::default(), Vec3::Y),
+            projection: OrthographicProjection {
+                scale: 0.01,
+                ..default()
+            }
+            .into(),
+            ..default()
+        },
+    ));
+}
